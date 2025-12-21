d33467a434f79fcbbc2d99c22e670d4e335bc282
Alan <361882@uwr.edu.pl>
2025-12-21 14:35:34 +0100

    Poprawienie logiki wypisywania planszy. Dodanie kolorów. Od teraz plansza jest wyśrodkowana w centrum terminalu oraz wcześniejszy tekst jest usuwany. Rozważanie nad zmianą szaty graficznej

--- output.c
+++ output.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 //#include <uchar.h>
 //#include "lib.h"
 
@@ -6,67 +8,112 @@
 // "┌───┐ " "┌─┬─┐ " "╔═══╗ " "╔═╤═╗ "
 // "└───┘ " "└─┴─┘ " "╚═══╝ " "╚═╧═╝ "
 // "│   │ " "├   ┤ " "║   ║ " "╟   ╢ "
+// "🭗    🭢" "🬼    🭇" ⃞
+// "🭽▔▔▔▔🭾" "🭼▁▁▁▁🭿" "▏    ▕"
 
+typedef struct{
+    char map[8][8];
+}Game_info;
+
+Game_info symulowanie_planszy_z_pionkami();
+
+
+char* klocki[8][2]={ //tablica z klockami do budowy planszy, poki co w wielkim chaosie
+    {"🭗    🭢", "╔═══╗ "},
+    {"┌─┬─┐ ", "╔═╤═╗ "},
+    {"🬼    🭇", "╚═══╝ "},
+    {"└─┴─┘ ", "╚═╧═╝ "},
+    {"├   ┤ ", "║   ║ "},
+    {"      ", "╟   ╢ "},
+    {"  ♜   ", " "},
+    {"  ♞   ", " "}
+};
+
+char* kolory[]={ //tablica ze wszystkimi kolorkami
+    "\x1b[48;2;219;160;125m", //jasny-plansza
+    "\x1b[48;2;117;55;13m", //ciemny-plansza
+    "\x1b[38;2;92;41;10m", //jasny-pionki
+    "\x1b[38;2;43;19;3m", //ciemny-pionki
+    "\x1b[0m" //brak
+};
 
 
 void set_output()
 {
-    //tablica ktora bedzie przechowywac wszystkie klocki do budowy planszy, poki co nie uwzglednia przeciwnikow i gdzie sie porusza
-    char* tablica_klockow[6][2]={
-        {"┌───┐ ", "╔═══╗ "},
-        {"┌─┬─┐ ", "╔═╤═╗ "},
-        {"└───┘ ", "╚═══╝ "},
-        {"└─┴─┘ ", "╚═╧═╝ "},
-        {"│   │ ", "║   ║ "},
-        {"├   ┤ ", "╟   ╢ "}
-    };
-    char *plansza_w_budowie[8*3][8];
-    
-    //petla nr 1: sprawdza z jakich elementow maja skladac sie pola na planszy i je zapisuje
-    for (int i=0; i<8;i++)
-    {
-        for (int k=0;k<8;k++)
-        {
-            //do zrobienia: sprawdzanie czy pole i,k zawiera ktoras figure/miejsce, gdzie poruszy sie przeciwnik/miejsce do ruszenia sie skoczka/
-            //do zrobienia: wstawienie do odpowiednika i,k wlasciwego klocka
-            //poki co: budowanie pustej planszy
-            plansza_w_budowie[i*3+0][k]=tablica_klockow[0][0];
-            plansza_w_budowie[i*3+1][k]=tablica_klockow[4/*+cos*/][0];
-            plansza_w_budowie[i*3+2][k]=tablica_klockow[2][0];
-        }
-        printf("\n");
-    }
+    //usuwanie tego co bylo w terminalu
+    printf("\e[1;1H\e[2J\e[3J");
 
-    //petla nr 2: wypisuje plansze
+
+    //obczajanie rozmiaru terminala
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    int odstep = (w.ws_col - 6*8) / 2;
+    if (odstep < 0) odstep=0;
+
+
+    Game_info pionki= symulowanie_planszy_z_pionkami();
+    
+    //petla co wypisuje plansze
     for (int i=0; i<24;i++)
     {
-        for (int j=0;j<8;j++)
+        printf("%*s ", odstep, klocki[7][1]);
+        for (int k=0;k<8;k++)
         {
-            //do zrobienia: kolorki
-            printf("%s", plansza_w_budowie[i][j]);
+            //do zrobienia: usunac te glupie ify
+            //do zrobienia: uwzglednic reszte figur
+            if (pionki.map[i/3][k]==0)
+            {
+                if (i%3==1) printf("%s%s%s" "\x1b[0m", kolory[(i/3+k)%2], kolory[3], klocki[5][0]);
+                else printf("%s%s%s" "\x1b[0m", kolory[(i/3+k)%2], kolory[3],klocki[i%3][0]);
+            }
+            if (pionki.map[i/3][k]==1)
+            {
+                if (i%3==1) printf("%s%s" "\x1b[0m", kolory[(i/3+k)%2], klocki[7][0]);
+                else printf("%s%s%s" "\x1b[0m", kolory[(i/3+k)%2], kolory[3], klocki[i%3][0]);
+            }
+            if (pionki.map[i/3][k]==2)
+            {
+                if (i%3==1) printf("%s%s" "\x1b[0m", kolory[(i/3+k)%2], klocki[6][0]);
+                else printf("%s%s%s" "\x1b[0m", kolory[(i/3+k)%2], kolory[3],klocki[i%3][0]);
+            }
         }
         printf("\n");
     }
 }
 
 //pomysly do uwzglednienia:
-//moze lepiej zrobic jedna petle i wypisywac od razu przy sprawdzeniu??? chyba oszczedniejsze, ale wydaje mi sie to byc brzydkim rozwiazaniem
-//pytanie: jak w map[8][8] zapisac, ze na jednym polu jest i przeciwnik, i inny przeciwnik chce sie tam poruszyc, i jeszcze skoczek moze tam skoczyc??? moze binarnie?? np 01000010 - wieza i mozna skoczyc
-//serio ktos to czyta??? jesli tak, to pozdro
-//jesli czyta to Andrei, to super robota z tymi lib.h i reszta
-//czy drugi rząd danego pola budować z trzech różnych elementów (sciana, pionek, sciana) czy przygotować elementy na kazdego pionka???
+//zmniejszyc kazde pole na planszy, bo pionki sa za male. Druga opcja - zwiekszyc pionki, ale to duzo roboty
 //jak szybko skoncze - animacja?? wyswietla sie najpierw plansza z samymi figurami, potem dodatkowo gdzie celuja przeciwnicy, a na koniec gdzie moze skoczyc skoczek
 
 
-/*
+
 int main()
 {
     int test;
-    scanf("%d", &test);
-    set_output();
+    while (1)
+    {
+        scanf("%d", &test);
+        set_output();
+    }
     test=test;
     return 0;
 }
-    */
+    
+
+Game_info symulowanie_planszy_z_pionkami()
+{
+    Game_info symulacja;
+    for (int i=0; i<8;i++)
+    {
+        for (int j=0;j<8;j++)
+        {
+            if ((i+j)%12==0) symulacja.map[i][j]=2;
+            //else if ((i+j)%7==0) pionki.map[i][j]=3;
+            else symulacja.map[i][j]=0;
+        }
+    }
+    symulacja.map[3][6]=1;
+    return symulacja;
+}
 
 
