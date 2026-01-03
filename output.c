bca952eec0ac22df50626163a00c4c4e185f8ba9
Alan <361882@uwr.edu.pl>
2026-01-03 11:58:43 +0100

    Od teraz kolory i znaki unicode powinny wyświetać się również na windowsie. Wyłączenie wyśrodkowania planszy. Parę poprawek w kodzie i grafice

--- output.c
+++ output.c
@@ -1,15 +1,20 @@
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <sys/ioctl.h>
+    #include <unistd.h>
+#endif
+
 #include <stdio.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
-//#include <uchar.h>
 #include "lib.h"
 
-void wsadzanie_przeciwnikow_na_plansze(Game_info*);
-void wsadzanie_ruchu_przeciwnikow(Game_info*);
-Game_info obliczanie_mozliwych_ruchow(int, int);
+void windows_dzialaj();
+void wsadzanie_przeciwnikow_na_plansze(char[8][8]);
+void wsadzanie_ruchu_przeciwnikow(char[8][8]);
+void obliczanie_mozliwych_ruchow(int, int, char[8][8]);
 
 //tablica z klockami do budowy planszy:
-char* klocki[25]={
+char* klocki[27]={
     "🭗      🭢",
     "        ",
     "        ",
@@ -20,21 +25,23 @@ char* klocki[25]={
     "◖◗", //[7]
     "🭃🭎",
     "🬪🬜",
-    "🭃🭎",
+    "🭁🭌",
     "🭆🭩",
-    "🭵🭐",
+    "🭵🭪",
     "🮭🮬",
     "🭅🭐",
     "🭩🭩",
-    "🭖🭡",
+    "🭨🭪",
     " ", //[17]
     "  ⏷  ⏷  ",
     "⏵ ",
     " ⏴",
     "  ⏶  ⏶  ",
     " ",
-    "\\",
-    "/"
+    "🮢",//[23]
+    "🮠",
+    "🮣",
+    "🮡",
 };
 
 char* kolory[6]={ //tablica ze wszystkimi kolorkami:
@@ -50,36 +57,39 @@ char* kolory[6]={ //tablica ze wszystkimi kolorkami:
 
 void set_output()
 {
-    Game_info pionki;
-    wsadzanie_przeciwnikow_na_plansze(&pionki);
-    Game_info ataki;
-    wsadzanie_ruchu_przeciwnikow(&ataki);
-    pionki.x=-1;
-    Game_info ruchy= obliczanie_mozliwych_ruchow(pionki.x,pionki.y); //poki co nie robi nic :/
+    char pionki[8][8];
+    wsadzanie_przeciwnikow_na_plansze(pionki);
+    char ataki[8][8];
+    wsadzanie_ruchu_przeciwnikow(ataki);
+    char skoczek_x=-1;
+    char skoczek_y=-1;
+    char ruchy [8][8];
+    obliczanie_mozliwych_ruchow(skoczek_x, skoczek_y, ruchy); //poki co nie robi nic :/
 
 
     //usuwanie tego co bylo w terminalu
     printf("\e[1;1H\e[2J\e[3J");
 
-    //obczajanie rozmiaru terminala
+    /*obczajanie rozmiaru terminala
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
     int odstep = (w.ws_col - 8*8) / 2;
     if (odstep < 0) odstep=0;
+    */
 
 
     //petla co wypisuje plansze
     for (int i=0; i<32;i++)
     {
-        printf("%*s ", odstep, klocki[17]);
+        //printf("%*s ", odstep, klocki[17]);
         for (int k=0;k<8;k++)
         {
             if(i%4==0 || i%4==3)
             {
-                printf("%s%s%s" "\x1b[0m", kolory[(i/4+k)%2], kolory[3 -ruchy.map[i/4][k] ],klocki[i%4 + ruchy.map[i/4][k]*18]);
+                printf("%s%s%s" "\x1b[0m", kolory[(i/4+k)%2], kolory[3 -ruchy[i/4][k] ],klocki[i%4 + ruchy[i/4][k]*18]);
             } 
-            else if(i%4==1) printf("%s" "%s%s%s" "%s%s" "%s%s%s" "\x1b[0m", kolory[(i/4+k)%2], kolory[3 -ruchy.map[i/4][k] ], klocki[4 + ruchy.map[i/4][k]*15], klocki[22+ataki.map[i/4][k]], kolory[pionki.map[i/4][k]==-1? 2 : 3], klocki[5+0+2*pionki.map[i/4][k]], kolory[3 -ruchy.map[i/4][k] ], klocki[22+2*ataki.map[i/4][k]], klocki[4 + ruchy.map[i/4][k]*16]);
-            else printf("%s" "%s%s%s" "%s%s" "%s%s%s" "\x1b[0m", kolory[(i/4+k)%2], kolory[3 -ruchy.map[i/4][k] ], klocki[4 + ruchy.map[i/4][k]*15], klocki[22+2*ataki.map[i/4][k]], kolory[pionki.map[i/4][k]==-1? 2 : 3], klocki[5+1+2*pionki.map[i/4][k]], kolory[3 -ruchy.map[i/4][k] ], klocki[22+ataki.map[i/4][k]], klocki[4 + ruchy.map[i/4][k]*16]);
+            else if(i%4==1) printf("%s" "%s%s%s" "%s%s" "%s%s%s" "\x1b[0m", kolory[(i/4+k)%2], kolory[3 -ruchy[i/4][k] ], klocki[4 + ruchy[i/4][k]*15], klocki[22+ataki[i/4][k]], kolory[pionki[i/4][k]==-1? 2 : 3], klocki[5+0+2*pionki[i/4][k]], kolory[3 -ruchy[i/4][k] ], klocki[22+3*ataki[i/4][k]], klocki[4 + ruchy[i/4][k]*16]);
+            else printf("%s" "%s%s%s" "%s%s" "%s%s%s" "\x1b[0m", kolory[(i/4+k)%2], kolory[3 -ruchy[i/4][k] ], klocki[4 + ruchy[i/4][k]*15], klocki[22+2*ataki[i/4][k]], kolory[pionki[i/4][k]==-1? 2 : 3], klocki[5+1+2*pionki[i/4][k]], kolory[3 -ruchy[i/4][k] ], klocki[22+4*ataki[i/4][k]], klocki[4 + ruchy[i/4][k]*16]);
         }
         printf("\n");
     }
@@ -94,49 +104,63 @@ void set_output()
 
 
 //funkcje pomocnicze:
-void wsadzanie_przeciwnikow_na_plansze(Game_info *pionki)
+void wsadzanie_przeciwnikow_na_plansze(char pionki[8][8])
 {
     for (int i=0; i<8;i++)
         for (int j=0; j<8;j++)
-            pionki->map[i][j]=0;
+            pionki[i][j]=0;
 
     for (int i=0; i<64;i++)
         if (enemies[i].alive == 1 && enemies[i].curr_x>=0 && enemies[i].curr_y>=0)
-            pionki->map[enemies[i].curr_x][enemies[i].curr_y]=enemies[i].figure+1;
+            pionki[enemies[i].curr_x][enemies[i].curr_y]=enemies[i].figure+1;
 
     return;
 }
 
-void wsadzanie_ruchu_przeciwnikow(Game_info* ataki)
+void wsadzanie_ruchu_przeciwnikow(char ataki[8][8])
 {
     for (int i=0; i<8;i++)
         for (int j=0; j<8;j++)
-            ataki->map[i][j]=0;
+            ataki[i][j]=0;
 
     for (int i=0; i<64;i++)
         if (enemies[i].alive == 1 && enemies[i].next_x>=0 && enemies[i].next_y>=0 && enemies[i].next_x<8 && enemies[i].next_y<8)
-            ataki->map[enemies[i].next_x][enemies[i].next_y]=1;
+            ataki[enemies[i].next_x][enemies[i].next_y]=1;
 
     return;
 }
     
-Game_info obliczanie_mozliwych_ruchow(int x, int y)
+void obliczanie_mozliwych_ruchow(int x, int y, char skoczek[8][8])
 {
-    Game_info skoczek;
     for (int i=0; i<8;i++)
         for (int j=0;j<8;j++)
-            skoczek.map[i][j]=0;
+            skoczek[i][j]=0;
 
-    if (x==-1) return skoczek;
-    if (x+2<8 && y+1<8) skoczek.map[x+2][y+1]=1;
-    if (x+2<8 && y-1>-1) skoczek.map[x+2][y-1]=1;
-    if (x-2>-1 && y+1<8) skoczek.map[x-2][y+1]=1;
-    if (x-2>-1 && y-1>-1) skoczek.map[x-2][y-1]=1;
-    if (x+1<8 && y+2<8) skoczek.map[x+1][y+2]=1;
-    if (x+1<8 && y-2>-1) skoczek.map[x+1][y-2]=1;
-    if (x-1>-1 && y+2<8) skoczek.map[x-1][y+2]=1;
-    if (x-1>-1 && y-2>-1) skoczek.map[x-1][y-2]=1;
-    skoczek.map[x][y]=1;
-    return skoczek;
+    if (x==-1) return;
+    if (x+2<8 && y+1<8) skoczek[x+2][y+1]=1;
+    if (x+2<8 && y-1>-1) skoczek[x+2][y-1]=1;
+    if (x-2>-1 && y+1<8) skoczek[x-2][y+1]=1;
+    if (x-2>-1 && y-1>-1) skoczek[x-2][y-1]=1;
+    if (x+1<8 && y+2<8) skoczek[x+1][y+2]=1;
+    if (x+1<8 && y-2>-1) skoczek[x+1][y-2]=1;
+    if (x-1>-1 && y+2<8) skoczek[x-1][y+2]=1;
+    if (x-1>-1 && y-2>-1) skoczek[x-1][y-2]=1;
+    skoczek[x][y]=1;
+    return;
+}
+
+void windows_dzialaj() 
+{
+#ifdef _WIN32
+    //UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+
+    //kolorki
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+#endif
 }
 
