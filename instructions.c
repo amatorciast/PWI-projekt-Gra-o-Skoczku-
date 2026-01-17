0a52b5bf636964a720fa40ad34ba006be4ab1186
kubasroka28 <kubasroka28@gmail.com>
2026-01-17 15:07:58 +0100

    Zmiana klawiszy do poruszania. Stwierdziłem, że porządek zgodny z godzinami na zegarze jest bardziej intuicyjny. Jeżeli komuś się nie spododa zmiana to można cofnąć.

--- instructions.c
+++ instructions.c
@@ -5,11 +5,11 @@ void show_instructions(){
     printf("Oto instrukcja poruszania się. Numer w danej pozycji oznacza, że klikcięcio go wykona ruch skoczka na tę pozycję\n");
     printf("#-------#\n");
     printf("|       |\n");
-    printf("|  3 2  |\n");
-    printf("| 4   1 |\n");
+    printf("|  8 1  |\n");
+    printf("| 7   2 |\n");
     printf("|   0   |\n");
-    printf("| 5   8 |\n");
-    printf("|  6 7  |\n");
+    printf("| 6   3 |\n");
+    printf("|  5 4  |\n");
     printf("|       |\n");
     printf("#-------#\n");
     printf("Klawisz 'p' otwiera menu pauzy\n\n");
--- player.c
+++ player.c
@@ -5,52 +5,59 @@
 void player_move(int direction) {
     switch (direction) {
         case 1:
+        if (game.x >= 2 && game.y <= 6) {
+            game.x -= 2;
+            game.y ++;
+        }        
+        break;
+
+        case 2:
         if (game.x >= 1 && game.y <= 6) {
             game.x --;
             game.y += 2;
         }
         break;
-        case 2:
-        if (game.x >= 2 && game.y <= 6) {
-            game.x -= 2;
-            game.y ++;
-        }
-        break;
+
         case 3:
-        if (game.x >= 2 && game.y >= 1) {
-            game.x -= 2;
-            game.y --;
-        }
-        break;
-        case 4:
-        if (game.x >= 1 && game.y >= 2) {
-            game.x --;
-            game.y -= 2;
-        }
-        break;
-        case 5:
-        if (game.x <= 6 && game.y >= 2) {
+        if (game.x <= 6 && game.y <= 5) {
             game.x ++;
-            game.y -= 2;
+            game.y += 2;
         }
         break;
-        case 6:
-        if (game.x <= 5 && game.y >= 1) {
-            game.x += 2;
-            game.y --;
-        }
-        break;
-        case 7:
+
+        case 4:
         if (game.x <= 5 && game.y <= 6) {
             game.x += 2;
             game.y ++;
         }
         break;
-        case 8:
-        if (game.x <= 6 && game.y <= 5) {
-            game.x ++;
-            game.y += 2;
+
+        case 5:
+        if (game.x <= 5 && game.y >= 1) {
+            game.x += 2;
+            game.y --;
         }
+        break;
+
+        case 6:
+        if (game.x <= 6 && game.y >= 2) {
+            game.x ++;
+            game.y -= 2;
+        }        
+        break;
+
+        case 7:
+        if (game.x >= 1 && game.y >= 2) {
+            game.x --;
+            game.y -= 2;
+        } 
+        break;
+
+        case 8:
+        if (game.x >= 2 && game.y >= 1) {
+            game.x -= 2;
+            game.y --;
+        } 
         break;
     }
 
